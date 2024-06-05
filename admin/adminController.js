102d6415936ee013d1ecbaf3102125ef5df6d4c1
_jATMOS <bks.mhryadayada@gmail.com>
2024-06-05 06:33:33 +0545

    Revert "catch error block and error handling"
    This reverts commit 61a5423e5d2e0f7b8c67c80067dc05577171289d.

--- admin/adminController.js
+++ admin/adminController.js
@@ -1,40 +1,58 @@
 const express = require("express");
 const worker = require("./adminModel");
-const catchAsync = require("./../utils/catchAsync");
 
 // creating a worker
-exports.createWorker = catchAsync(async (req, res) => {
-  const newWorker = await worker.create(req.body);
-  res.status(201).json({
-    status: "success",
-    data: {
-      worker: newWorker,
-    },
-  });
-});
+exports.createWorker = async (req, res) => {
+  try {
+    const newWorker = await worker.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        worker: newWorker,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed to create worker",
+      message: err.message,
+    });
+  }
+};
 
 // update the workerdetails
-exports.updateWorker = catchAsync(async (req, res) => {
-  const updateWorker = await worker.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.status(200).json({
-    status: "sucess",
-    result: "updated worker detail",
-    data: {
-      updateWorker,
-    },
-  });
-});
+exports.updateWorker = async (req, res) => {
+  try {
+    const updateWorker = await worker.findByIdAndUpdate(req.params.id,req.body,{new:true})
+    res.status(200).json({
+      status: "sucess",
+      result: "updated worker detail",
+      data: {
+        updateWorker,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
 // delete the worker
-exports.deleteWorker = catchAsync(async (req, res) => {
-  const updateWorker = await worker.findByIdAndDelete(req.params.id);
-  res.status(200).json({
-    status: "sucess",
-    result: "Deleted worker detail",
-    data: {
-      updateWorker,
-    },
-  });
-});
+exports.deleteWorker = async (req, res) => {
+  try {
+    const updateWorker = await worker.findByIdAndDelete(req.params.id)
+    res.status(200).json({
+      status: "sucess",
+      result: "Deleted worker detail",
+      data: {
+        updateWorker,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
--- commonController.js
+++ commonController.js
@@ -1,55 +1,69 @@
 const { json } = require("express");
 const worker = require("./admin/adminModel");
-const catchAsync = require("./utils/catchAsync");
 
 // getting all the workers with filtering and sorting and pagination
-exports.getWorkers = catchAsync(async (req, res) => {
-  // filtering
-  const queryObj = { ...req.query };
-  const excludedFields = ["page", "sort", "limit", "fields"];
-  excludedFields.forEach((el) => delete queryObj[el]);
+exports.getWorkers = async (req, res) => {
+  try {
+    // filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-  // advanced filtering
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-  let query = worker.find(JSON.parse(queryStr));
+    let query = worker.find(JSON.parse(queryStr));
 
-  // sorting
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
+    // sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    }
+
+    //pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 20;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(10);
+    if (req.query.page) {
+      const numWorkers = await worker.countDocuments();
+      if (skip >= numWorkers) throw new Error("this page does not exist");
+    }
+    // finding the workers
+    const findingWorkers = await query;
+    res.status(200).json({
+      status: "sucess",
+      result: "found all the workers",
+      data: {
+        findingWorkers,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "failed to get the worker detail",
+      message: err.message,
+    });
   }
-
-  //pagination
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 20;
-  const skip = (page - 1) * limit;
-  query = query.skip(skip).limit(10);
-  if (req.query.page) {
-    const numWorkers = await worker.countDocuments();
-    if (skip >= numWorkers) throw new Error("this page does not exist");
-  }
-  // finding the workers
-  const findingWorkers = await query;
-  res.status(200).json({
-    status: "sucess",
-    result: "found all the workers",
-    data: {
-      findingWorkers,
-    },
-  });
-});
+};
 
 // get an worker
-exports.getWorker = catchAsync(async (req, res) => {
-  const findingAWorker = await worker.findById(req.params.id);
-  res.status(200).json({
-    status: "sucess",
-    result: "found",
-    data: {
-      findingAWorker,
-    },
-  });
-});
+exports.getWorker = async (req, res) => {
+  try {
+    const findingAWorker = await worker.findById(req.params.id);
+    res.status(200).json({
+      status: "sucess",
+      result: "found",
+      data: {
+        findingAWorker,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "failed to get the worker detail",
+      message: err.message,
+    });
+  }
+};
 
+//filtering workers
