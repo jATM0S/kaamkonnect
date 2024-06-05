68d84f38a072e7e68e0b4d3f106d4471382f3d3c
_jATMOS <bks.mhryadayada@gmail.com>
2024-06-05 12:58:36 +0545

    error handling and handling no id error

--- admin/adminController.js
+++ admin/adminController.js
@@ -1,58 +1,48 @@
 const express = require("express");
 const worker = require("./adminModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+
 
 // creating a worker
-exports.createWorker = async (req, res) => {
-  try {
-    const newWorker = await worker.create(req.body);
-    res.status(201).json({
-      status: "success",
-      data: {
-        worker: newWorker,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "failed to create worker",
-      message: err.message,
-    });
-  }
-};
+exports.createWorker = catchAsync(async (req, res, next) => {
+  const newWorker = await worker.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      worker: newWorker,
+    },
+  });
+});
 
 // update the workerdetails
-exports.updateWorker = async (req, res) => {
-  try {
-    const updateWorker = await worker.findByIdAndUpdate(req.params.id,req.body,{new:true})
-    res.status(200).json({
-      status: "sucess",
-      result: "updated worker detail",
-      data: {
-        updateWorker,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err.message,
-    });
+exports.updateWorker = catchAsync(async (req, res, next) => {
+  const updateWorker = await worker.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!updateWorker){
+    return next(new AppError('No worker of that ID!!',404))
   }
-};
+  res.status(200).json({
+    status: "sucess",
+    result: "updated worker detail",
+    data: {
+      updateWorker,
+    },
+  });
+});
 
 // delete the worker
-exports.deleteWorker = async (req, res) => {
-  try {
-    const updateWorker = await worker.findByIdAndDelete(req.params.id)
-    res.status(200).json({
-      status: "sucess",
-      result: "Deleted worker detail",
-      data: {
-        updateWorker,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err.message,
-    });
+exports.deleteWorker = catchAsync(async (req, res, next) => {
+  const updateWorker = await worker.findByIdAndDelete(req.params.id);
+  if (!deleteWorker){
+    return next(new AppError('No worker of that ID!!',404))
   }
-};
+  res.status(200).json({
+    status: "sucess",
+    result: "Deleted worker detail",
+    data: {
+      updateWorker,
+    }, 
+  });
+});
--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
-
+const AppError = require("./utils/appError");
+const globalErrorHandler=require('./utils/errorController')
 app.use(express.json());
 
 const router = require("./admin/routers");
 app.use("/admin", router);
 
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 module.exports = app;
