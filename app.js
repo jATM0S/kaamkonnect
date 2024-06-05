61a5423e5d2e0f7b8c67c80067dc05577171289d
_jATMOS <bks.mhryadayada@gmail.com>
2024-06-05 06:20:26 +0545

    catch error block and error handling

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
-
+const AppError = require("./utils/errorHandler");
+const globalErrorHandler = require("./utils/globalErrorHandler");
 app.use(express.json());
 
 const router = require("./admin/routers");
 app.use("/admin", router);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server!`, 404));
+});
+app.use(globalErrorHandler);
 
 module.exports = app;
