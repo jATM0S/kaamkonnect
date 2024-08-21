c7001e110a08140ea7330371d97cf9121c9c3986
_jATMOS <bks.mhryadayada@gmail.com>
2024-08-21 22:30:49 +0545

    ejs configure basic template

--- app.js
+++ app.js
@@ -4,16 +4,25 @@ const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./utils/errorController");
+const path = require('path');
+
 app.use(express.json());
 
 //data sanitization against query injection
 app.use(mongoSanitize());
 //sanitization against xss attact
 app.use(xss());
- 
+
+//view stuff
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.get("/", (req, res) => {
+  res.status(200).render("pages/base");
+});
+
+//backend routes stuff
 const router = require("./routers/routers");
 app.use("/", router);
-
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
