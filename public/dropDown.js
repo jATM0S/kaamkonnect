05b2205a21ab068f3a03d9417f515594292c5f9c
_jATMOS <bks.mhryadayada@gmail.com>
2024-09-05 08:41:52 +0545

    worker setting change password and delete account

--- public/dropDown.js
+++ public/dropDown.js
@@ -4,3 +4,13 @@ function toggleSettingDropdown() {
     dropdownMenu.classList.toggle("hidden");
   }
 }
+function toggleNavMenu() {
+  const dropdownNav = document.getElementById("dropdownNav");
+  if (window.innerWidth < 768) {
+    dropdownNav.classList.toggle("hidden");
+    const openIcon = document.getElementById("menu-open-icon");
+    const closeIcon = document.getElementById("menu-close-icon");
+    openIcon.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+  }
+}
